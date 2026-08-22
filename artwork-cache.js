63a082aa580746441e654717367ba2ad72d4632a
tangkk <dengjunqi06323011@gmail.com>
2026-08-23 02:05:44 +0800

    Remove unused artwork cache registrar

--- artwork-cache.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  if (!('serviceWorker' in navigator)) return;
-
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./artwork-cache-sw.js', { scope: './' }).catch(() => {});
-  });
-})();
