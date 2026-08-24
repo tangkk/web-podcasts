cb4f28d61b63eb29702da05c7dfb54a27c15fc57
tangkk <dengjunqi06323011@gmail.com>
2026-08-24 21:45:43 +0800

    Normalize NPR artwork source URLs

--- artwork-url-fix.js
+++ artwork-url-fix.js
@@ -2,6 +2,8 @@
   const nativeFetch = window.fetch.bind(window);
   const BBC_ARTWORK_HTTP = 'http://ichef.bbci.co.uk/';
   const BBC_ARTWORK_HTTPS = 'https://ichef.bbci.co.uk/';
+  const NPR_ARTWORK_HTTP = 'url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com';
+  const NPR_ARTWORK_HTTPS = 'url=https%3A%2F%2Fnpr-brightspot.s3.amazonaws.com';
 
   function shouldRewrite(url) {
     try {
@@ -18,12 +20,17 @@
     if (!response.ok || !shouldRewrite(rawUrl)) return response;
 
     const text = await response.clone().text();
-    if (!text.includes(BBC_ARTWORK_HTTP)) return response;
+    if (!text.includes(BBC_ARTWORK_HTTP) && !text.includes(NPR_ARTWORK_HTTP)) return response;
 
-    return new Response(text.replaceAll(BBC_ARTWORK_HTTP, BBC_ARTWORK_HTTPS), {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers
-    });
+    return new Response(
+      text
+        .replaceAll(BBC_ARTWORK_HTTP, BBC_ARTWORK_HTTPS)
+        .replaceAll(NPR_ARTWORK_HTTP, NPR_ARTWORK_HTTPS),
+      {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers
+      }
+    );
   };
 })();
